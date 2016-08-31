fb1e27bd9278f249a4b2549b0c72ec30c5cd6d0f
Sam <freakyforester@gmail.com>
2016-08-31 13:23:41 -0500

    SQL Update
    Forgot to release this last night

--- freets3v2.sql
+++ freets3v2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.0.10.14
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 31, 2016 at 05:11 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.5.37
+-- Host: localhost:3306
+-- Generation Time: Aug 31, 2016 at 02:22 PM
+-- Server version: 5.5.49-cll-lve
+-- PHP Version: 5.6.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `freets3v2`
@@ -23,10 +23,22 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `registerTokens`
+--
+
+CREATE TABLE IF NOT EXISTS `registerTokens` (
+  `token` varchar(40) NOT NULL COMMENT 'The Unique Token Generated',
+  `uid` int(11) NOT NULL COMMENT 'The User Id',
+  `requested` varchar(20) NOT NULL COMMENT 'The Date when token was created'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `resettokens`
 --
 
-CREATE TABLE `resettokens` (
+CREATE TABLE IF NOT EXISTS `resettokens` (
   `token` varchar(40) NOT NULL COMMENT 'The Unique Token Generated',
   `uid` int(11) NOT NULL COMMENT 'The User Id',
   `requested` varchar(20) NOT NULL COMMENT 'The Date when token was created'
@@ -38,7 +50,7 @@ CREATE TABLE `resettokens` (
 -- Table structure for table `servers`
 --
 
-CREATE TABLE `servers` (
+CREATE TABLE IF NOT EXISTS `servers` (
   `location` text NOT NULL,
   `ip` varchar(50) NOT NULL,
   `query` varchar(500) NOT NULL,
@@ -53,8 +65,8 @@ CREATE TABLE `servers` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(10) NOT NULL,
   `email` tinytext NOT NULL,
   `password` varchar(64) NOT NULL,
@@ -63,8 +75,10 @@ CREATE TABLE `users` (
   `created` datetime NOT NULL,
   `attempt` varchar(15) NOT NULL DEFAULT '0',
   `ipaddress` varchar(500) NOT NULL,
-  `servermade` int(1) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `confirmed` int(1) NOT NULL COMMENT 'Confirmed email address',
+  `servermade` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 -- --------------------------------------------------------
 
@@ -72,7 +86,7 @@ CREATE TABLE `users` (
 -- Table structure for table `userservers`
 --
 
-CREATE TABLE `userservers` (
+CREATE TABLE IF NOT EXISTS `userservers` (
   `user_id` int(50) NOT NULL,
   `ip` varchar(50) NOT NULL,
   `port` int(255) NOT NULL,
@@ -85,43 +99,14 @@ CREATE TABLE `userservers` (
 -- Table structure for table `user_devices`
 --
 
-CREATE TABLE `user_devices` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user_devices` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) NOT NULL COMMENT 'The user''s ID',
   `token` varchar(15) NOT NULL COMMENT 'A unique token for the user''s device',
-  `last_access` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `last_access` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user_devices`
---
-ALTER TABLE `user_devices`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `user_devices`
---
-ALTER TABLE `user_devices`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
