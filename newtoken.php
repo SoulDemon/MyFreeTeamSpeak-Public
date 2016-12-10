2ca4d15a4cd0263e1939eeadc0fb659fffc4bb16
BayXx <hampus.cc@gmail.com>
2016-12-10 03:03:40 +0100

    Updated
    + Added template style
    + Added Alert Success & Alert Danger, depends on output
    + Added available Server Tokens list, as a quick overview
    - Changed button name and Success output. Looks better
    - Removed all old code. Black and white are boring ;)

--- newtoken.php
+++ newtoken.php
@@ -8,37 +8,148 @@ if( isset($_POST['newName']) ){
 }
 ?>
 <!DOCTYPE html>
-<html>
-  <head></head>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+
+    <title>MyFreeTeamSpeak</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="assets/css/zabuto_calendar.css">
+    <link rel="stylesheet" type="text/css" href="assets/js/gritter/css/jquery.gritter.css" />
+    <link rel="stylesheet" type="text/css" href="assets/lineicons/style.css">    
+    
+    <!-- Custom styles for this template -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/style-responsive.css" rel="stylesheet">
+
+    <script src="assets/js/chart-master/Chart.js"></script>
+    
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
   <body>
-    <div class="content">
-      <h1>Generate a new token for: <?php echo \Fr\LS::userName();?>?</h1>
-	  
-      <form action="newtoken.php" method="POST">
-              <label>
-          <button name="submit">Submit Request</button>
-        </label>
-      </form>
-	  	
-      <?php
-      if( isset($_POST['submit']) ){
-          $createAccount = \Fr\LS::newToken();	
-			if($createAccount === true){
-            echo "<label>Success Token Created!</label>";
-          }
-		  else
-		  {
-			  echo "<label>Something went horribly fucking wrong...</label>";
-		  }
-        
-      }
-      ?>
-      <style>
-        label{
-          display: block;
-          margin-bottom: 5px;
-        }
-      </style>
-    </div>
+
+  <section id="container" >
+      <!-- **********************************************************************************************************************************************************
+      TOP BAR CONTENT & NOTIFICATIONS
+      *********************************************************************************************************************************************************** -->
+      <!--header start-->
+      <header class="header black-bg">
+              <div class="sidebar-toggle-box">
+                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+              </div>
+            <!--logo start-->
+            <a href="index.php" class="logo"><b>MyFreeTeamSpeak</b></a>
+            <!--logo end-->
+            <div class="nav notify-row" id="top_menu">
+                <!--  notification start -->
+                <ul class="nav top-menu">
+
+                </ul>
+                <!--  notification end -->
+            </div>
+            <div class="top-menu">
+            	<ul class="nav pull-right top-menu">
+                    <li><a class="logout" href="logout.php">Logout</a></li>
+            	</ul>
+            </div>
+        </header>
+      <!--header end-->
+      
+      <!-- **********************************************************************************************************************************************************
+      MAIN SIDEBAR MENU
+      *********************************************************************************************************************************************************** -->
+       <?php
+	   include './sidebar.php';
+	   ?>
+	   
+	   <!-- **********************************************************************************************************************************************************
+      MAIN CONTENT
+      *********************************************************************************************************************************************************** -->
+      <!--main content start-->
+      <section id="main-content">
+          <section class="wrapper">
+
+              <div class="row">
+                  <div class="col-lg-9 main-chart">
+			<div class="content">
+				<h1>Generate a new token for: <?php echo \Fr\LS::userName();?>?</h1>
+						  
+				<form action="newtoken.php" method="POST">
+					<label>
+						<button name="submit">Generate</button>
+					</label>
+				 </form>
+							
+				<?php
+					if( isset($_POST['submit']) ){
+						$createAccount = \Fr\LS::newToken();	
+						if($createAccount === true){
+							echo "<br /><div class='alert alert-success' role='alert'>New Token Generated</div>";
+						}
+						else
+						{
+							echo "<br /><div class='alert alert-danger' role='alert'>Something went horribly fucking wrong...</div>";
+						}	
+					}
+				?>
+				<br/>
+				<pre>
+					<?php
+						try{
+							$tokenlist = \Fr\LS::returnServerKeys();
+							print_r($tokenlist);			
+						}
+						catch(Exception $e)
+						{
+							echo "No Keys Available";
+						}
+					?>
+				</pre>
+				<style>
+					label{
+						display: block;
+						margin-bottom: 5px;
+					}
+				</style>
+			</div><!-- Div Content End -->
+		</div><!-- /col-lg-9 END SECTION MIDDLE -->
+	</div><!-- Div Row End -->
+<!--main content end-->
+     
+  </section>
+
+    <!-- js placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/jquery-1.8.3.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="assets/js/jquery.scrollTo.min.js"></script>
+    <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script src="assets/js/jquery.sparkline.js"></script>
+
+
+    <!--common script for all pages-->
+    <script src="assets/js/common-scripts.js"></script>
+    
+    <script type="text/javascript" src="assets/js/gritter/js/jquery.gritter.js"></script>
+    <script type="text/javascript" src="assets/js/gritter-conf.js"></script>
+
+    <!--script for this page-->
+    <script src="assets/js/sparkline-chart.js"></script>    
+	<script src="assets/js/zabuto_calendar.js"></script>	
+	
   </body>
 </html>
